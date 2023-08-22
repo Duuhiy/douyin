08ccb81642f6e9a5b32ef897d5fd8bc75963683c
duuhiy <1429520750@qq.com>
2023-08-22 21:07:49 +0800

    增加social rpc中user转api中user的函数

---
+++ api/douyin/internal/utils/UserRPCS2API.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"douyin/api/douyin/internal/types"
+	"douyin/rpc/social/pb"
+)
+
+func UserRPCS2API(puser *pb.User) (user *types.User) {
+	return &types.User{
+		Id:               puser.Id,
+		Name:             puser.Name,
+		Follow_count:     puser.FollowCount,
+		Follower_count:   puser.FollowerCount,
+		Is_follow:        puser.IsFollow,
+		Avatar:           puser.Avatar,
+		Background_image: puser.BackgroundImage,
+		Signature:        puser.Signature,
+		Total_favorited:  puser.TotalFavorited,
+		Work_count:       puser.WorkCount,
+		Favorite_count:   puser.FavoriteCount,
+	}
+}
