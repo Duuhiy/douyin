f38337093585830d0839b3e454db5becc1257db7
duuhiy <1429520750@qq.com>
2023-08-22 17:49:02 +0800

    点赞api的基本逻辑

--- api/douyin/internal/handler/douyinFavorite/douyinFavoriteActionHandler.go
+++ api/douyin/internal/handler/douyinFavorite/douyinFavoriteActionHandler.go
@@ -15,35 +15,19 @@ import (
 func DouyinFavoriteActionHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req types.DouyinFavoriteActionRequest
-		vars := r.URL.Query()
-		if token, ok := vars["token"]; ok {
-			req.Token = token[0]
-		} else {
-			fmt.Println("token错误")
-			httpx.ErrorCtx(r.Context(), w, errors.New("token错误"))
+		req.Token = r.FormValue("token")
+		videoId, err := strconv.ParseInt(r.FormValue("video_id"), 10, 64)
+		if err != nil {
+			fmt.Println("video_id 格式错误", err)
+			httpx.ErrorCtx(r.Context(), w, errors.New("video_id 格式错误"))
 		}
-
-		if videoIdStr, ok := vars["video_id"]; ok {
-			videoId, err := strconv.ParseInt(videoIdStr[0], 10, 64)
-			if err != nil {
-				httpx.ErrorCtx(r.Context(), w, errors.New("video_id格式错误"))
-			}
-			req.Video_id = videoId
-		} else {
-			fmt.Println("video_id错误")
-			httpx.ErrorCtx(r.Context(), w, errors.New("video_id错误"))
+		req.Video_id = videoId
+		actionType, err := strconv.ParseInt(r.FormValue("action_type"), 10, 64)
+		if err != nil {
+			fmt.Println("action_type 格式错误", err)
+			httpx.ErrorCtx(r.Context(), w, errors.New("action_type 格式错误"))
 		}
-
-		if ActionTypeStr, ok := vars["action_type"]; ok {
-			ActionType, err := strconv.ParseInt(ActionTypeStr[0], 10, 64)
-			if err != nil {
-				httpx.ErrorCtx(r.Context(), w, errors.New("action_type格式错误"))
-			}
-			req.Action_type = int32(ActionType)
-		} else {
-			fmt.Println("action_type错误")
-			httpx.ErrorCtx(r.Context(), w, errors.New("action_type错误"))
-		}
+		req.Action_type = int32(actionType)
 
 		l := douyinFavorite.NewDouyinFavoriteActionLogic(r.Context(), svcCtx)
 		fmt.Println("调用 api DouyinFavoriteAction")
