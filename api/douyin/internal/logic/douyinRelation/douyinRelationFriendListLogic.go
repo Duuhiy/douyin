1d62630c3baf7c08a2caf90ed10ce78c65e4d088
duuhiy <1429520750@qq.com>
2023-08-22 21:04:55 +0800

    增加好友列表的api

--- api/douyin/internal/logic/douyinRelation/douyinRelationFriendListLogic.go
+++ api/douyin/internal/logic/douyinRelation/douyinRelationFriendListLogic.go
@@ -2,6 +2,9 @@ package douyinRelation
 
 import (
 	"context"
+	"douyin/api/douyin/internal/utils"
+	"douyin/rpc/social/pb"
+	"fmt"
 
 	"douyin/api/douyin/internal/svc"
 	"douyin/api/douyin/internal/types"
@@ -25,6 +28,27 @@ func NewDouyinRelationFriendListLogic(ctx context.Context, svcCtx *svc.ServiceCo
 
 func (l *DouyinRelationFriendListLogic) DouyinRelationFriendList(req *types.DouyinRelationFriendListRequest) (resp *types.DouyinRelationFriendListResponse, err error) {
 	// todo: add your logic here and delete this line
+	rpcReq := pb.DouyinRelationFriendListRequest{
+		Token:  req.Token,
+		UserId: req.User_id,
+	}
+	fmt.Println("调用 rpc DouyinRelationFollowerList")
+	rpcResp, err := l.svcCtx.SocialRpcClient.DouyinRelationFriendList(l.ctx, &rpcReq)
+	if err != nil {
+		fmt.Println("rpc 服务 DouyinRelationFollowerList 出错了")
+		return &types.DouyinRelationFriendListResponse{
+			Status_code: rpcResp.StatusCode,
+			Status_msg:  "rpc 服务 DouyinRelationFollowerList 出错了",
+		}, err
+	}
+	var userList []types.User
+	for _, u := range rpcResp.UserList {
+		user := utils.UserRPCS2API(u)
+		userList = append(userList, *user)
+	}
 
-	return
+	return &types.DouyinRelationFriendListResponse{
+		Status_code: rpcResp.StatusCode,
+		UserList:    userList,
+	}, nil
 }
